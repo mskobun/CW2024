293f3f67f3e5459f0425e1621cac3e2585b556ad
Maksim Skobun <max@skobun.me>
2024-11-30 01:00:15 +0800

    Actors: Better collision detection by trimming images
    Some of the PNG images used for actors have quite large transparent
    sides. Since the collision detection algorithm currently only considers
    bounds of the ImageView, no matter if it is transparent, this results in
    hitboxes much larger than the visible part of the image.
    
    Trimming the edges of the PNG assets also saves a bit of space.

--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -3,14 +3,14 @@ package com.example.demo;
 public class UserPlane extends FighterPlane {
 
 	private static final String IMAGE_NAME = "userplane.png";
-	private static final double Y_UPPER_BOUND = -40;
-	private static final double Y_LOWER_BOUND = 600.0;
+	private static final double Y_UPPER_BOUND = 0;
+	private static final double Y_LOWER_BOUND = 711.0;
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
-	private static final int IMAGE_HEIGHT = 150;
+	private static final int IMAGE_HEIGHT = 39;
 	private static final int VERTICAL_VELOCITY = 160;
-	private static final int PROJECTILE_X_POSITION = 110;
-	private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
+	private static final int PROJECTILE_X_POSITION = 161;
+	private static final int PROJECTILE_Y_POSITION_OFFSET = 25;
 	private int velocityMultiplier;
 	private int numberOfKills;
 
