2922de4b18b5d1d3db687f6bd3bf0a9c9309138b
Maksim Skobun <max@skobun.me>
2024-12-12 03:37:51 +0800

    AssetFactory: Remove unused import + redundant modifier

--- src/main/java/com/example/demo/AssetFactory.java
+++ src/main/java/com/example/demo/AssetFactory.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public interface AssetFactory {
-    public Image createImage(String imageName);
+    Image createImage(String imageName);
 }
