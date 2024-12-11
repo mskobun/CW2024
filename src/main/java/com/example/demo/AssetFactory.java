83dd93250d8c6bdf2dcff30040a4406aa9391fb5
Maksim Skobun <max@skobun.me>
2024-12-12 03:42:07 +0800

    Asset factories: Add javadoc

--- src/main/java/com/example/demo/AssetFactory.java
+++ src/main/java/com/example/demo/AssetFactory.java
@@ -2,6 +2,16 @@ package com.example.demo;
 
 import javafx.scene.image.Image;
 
+/**
+ * An interface for creating {@link Image} instances.
+ */
 public interface AssetFactory {
+    /**
+     * Creates an {@link Image} instance for the given image name.
+     *
+     * @param imageName the name of the image to create, typically representing
+     *                  a file name or resource path.
+     * @return the {@link Image} object corresponding to the given name.
+     */
     Image createImage(String imageName);
 }
--- src/main/java/com/example/demo/CachedAssetFactory.java
+++ src/main/java/com/example/demo/CachedAssetFactory.java
@@ -5,13 +5,35 @@ import javafx.scene.image.Image;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * An {@link AssetFactory} implementation that caches {@link Image} objects to optimize resource loading.
+ */
 public class CachedAssetFactory implements AssetFactory {
+    /**
+     * A cache for storing previously loaded images, with the image name as the key.
+     */
     private final Map<String, Image> imageCache = new HashMap<>();
     private final String imagePrefix;
 
+    /**
+     * Constructs a new {@code CachedAssetFactory} with the specified image prefix.
+     *
+     * @param imagePrefix the prefix to prepend to image names when locating resources.
+     */
     public CachedAssetFactory(String imagePrefix) {
        this.imagePrefix = imagePrefix;
     }
+
+    /**
+     * Retrieves or creates an {@link Image} based on the given image name.
+     *
+     * <p>If the image has already been loaded previously, it is retrieved from the cache.
+     * Otherwise, it is loaded from the specified resource path and added to the cache.</p>
+     *
+     * @param imageName the name of the image to create, typically representing a file name or resource path.
+     * @return the {@link Image} object corresponding to the given name.
+     */
     @Override
     public Image createImage(String imageName) {
         return imageCache.computeIfAbsent(imageName, this::loadImage);
