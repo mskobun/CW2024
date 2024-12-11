8e39569886f27500ee990c09482e39da7f19928d
Maksim Skobun <max@skobun.me>
2024-12-12 04:39:58 +0800

    PauseOverlay: Darken the background

--- src/main/java/com/example/demo/screen/level/hud/PauseOverlay.java
+++ src/main/java/com/example/demo/screen/level/hud/PauseOverlay.java
@@ -2,18 +2,16 @@ package com.example.demo.screen.level.hud;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 public class PauseOverlay {
-    private final VBox view;
+    private final StackPane view;
     private final Runnable onResume;
     private final Runnable onMainMenu;
 
     public PauseOverlay(Runnable onResume, Runnable onMainMenu) {
-        view = new VBox();
+        view = new StackPane();
         this.onResume = onResume;
         this.onMainMenu = onMainMenu;
         initializeView();
@@ -24,14 +22,20 @@ public class PauseOverlay {
     }
 
     private void initializeView() {
-        view.setSpacing(5);
-        view.setAlignment(Pos.CENTER);
+        // Half-transparent black background
+        view.setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 0, 0.5), CornerRadii.EMPTY, null)));
+
+        VBox buttonBox = new VBox();
+        buttonBox.setSpacing(5);
+        buttonBox.setAlignment(Pos.CENTER);
+        StackPane.setAlignment(buttonBox, Pos.CENTER);
+        view.getChildren().add(buttonBox);
 
         Button resumeButton = new Button("Resume");
         resumeButton.setOnAction(e -> onResume.run());
         Button mainMenuButton = new Button("Main Menu");
         mainMenuButton.setOnAction(e -> onMainMenu.run());
 
-        view.getChildren().addAll(resumeButton, mainMenuButton);
+        buttonBox.getChildren().addAll(resumeButton, mainMenuButton);
     }
 }
